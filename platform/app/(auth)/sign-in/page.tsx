25ee261feb21a05058b592954eb5c57d6aa0189c
RitikPadhy <ritipkp2002@gmail.com>
2025-03-28 11:38:14 -0700

    Resolved issues

--- platform/app/(auth)/sign-in/page.tsx
+++ platform/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ComponentName = () => {
+const Page = () => {
   return (
     <div>
       Page
@@ -8,4 +8,4 @@ const ComponentName = () => {
   );
 };
 
-export default ComponentName;
+export default Page;
