021d5e494dfb20342cb9653bd1bf718f5eb02925
RitikPadhy <ritipkp2002@gmail.com>
2025-04-01 12:33:30 -0700

    Home Pages changes

--- platform/lib/utils.ts
+++ platform/lib/utils.ts
@@ -1,6 +1,51 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { interviewCovers, mappings } from "@/constants";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const techIconBaseURL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons";
+
+// Removes .js, and spaces, and find its corresponding value by using this formatted key as the key
+const normalizeTechName = (tech: string) => {
+  const key = tech.toLowerCase().replace(/\.js$/, "").replace(/\s+/g, "");
+  return mappings[key as keyof typeof mappings];
+};
+
+const checkIconExists = async (url: string) => {
+  try {
+    const response = await fetch(url, { method: "HEAD" });
+    return response.ok; // Returns true if the icon exists
+  } catch {
+    return false;
+  }
+};
+
+export const getTechLogos = async (techArray: string[]) => {
+  // We pass the tech icons, and from which we get the urls to these tech icons
+  const logoURLs = techArray.map((tech) => {
+    const normalized = normalizeTechName(tech);
+    return {
+      tech,
+      url: `${techIconBaseURL}/${normalized}/${normalized}-original.svg`,
+    };
+  });
+
+  // Return the results, which is basically the url of the tech logos and the logo name
+  const results = await Promise.all(
+    logoURLs.map(async ({ tech, url }) => ({
+      tech,
+      url: (await checkIconExists(url)) ? url : "/tech.svg",
+    }))
+  );
+
+  return results;
+};
+
+// Choose a random index/cover from a list of covers and indexes given
+export const getRandomInterviewCover = () => {
+  const randomIndex = Math.floor(Math.random() * interviewCovers.length);
+  return `/covers${interviewCovers[randomIndex]}`;
+};
